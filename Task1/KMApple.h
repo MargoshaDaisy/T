ccfa2189ba89a0c223e34a5d7e42bd66b02db8e2
fpmi <fpmi@android-dece3ad3f11cfb47.wifi.bsu>
2015-09-05 12:00:57 +0300

    create class Apple

---
+++ Task1/KMApple.h
@@ -0,0 +1,13 @@
+//
+//  KMApple.h
+//  Task1
+//
+//  Created by fpmi on 05.09.15.
+//  Copyright (c) 2015 fpmi. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface KMApple : NSObject
+
+@end
---
+++ Task1/KMApple.m
@@ -0,0 +1,13 @@
+//
+//  KMApple.m
+//  Task1
+//
+//  Created by fpmi on 05.09.15.
+//  Copyright (c) 2015 fpmi. All rights reserved.
+//
+
+#import "KMApple.h"
+
+@implementation KMApple
+
+@end
