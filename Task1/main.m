da5454020805ac37a1f95d7b08ab2f779ea73d1b
fpmi <fpmi@android-dece3ad3f11cfb47.wifi.bsu>
2015-09-05 11:56:59 +0300

    Initial Commit

---
+++ Task1/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Task1
+//
+//  Created by fpmi on 05.09.15.
+//  Copyright (c) 2015 fpmi. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
